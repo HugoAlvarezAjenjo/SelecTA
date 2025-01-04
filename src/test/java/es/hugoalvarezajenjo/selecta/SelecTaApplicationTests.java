37c863c0c5c0667cdf18df2293158797a771ad70
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-01-04 18:12:14 +0100

    chore: Initial Commit

---
+++ src/test/java/es/hugoalvarezajenjo/selecta/SelecTaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.hugoalvarezajenjo.selecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelecTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
