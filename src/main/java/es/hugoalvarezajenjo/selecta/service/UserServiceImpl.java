3f25514e5a8a631e4b61bdbcdfc010c941354d5e
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2025-01-30 15:38:45 +0100

    feat: Authentication requirement

--- src/main/java/es/hugoalvarezajenjo/selecta/service/UserServiceImpl.java
+++ src/main/java/es/hugoalvarezajenjo/selecta/service/UserServiceImpl.java
@@ -2,26 +2,41 @@ package es.hugoalvarezajenjo.selecta.service;
 
 import es.hugoalvarezajenjo.selecta.entity.User;
 import es.hugoalvarezajenjo.selecta.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(final UserRepository userRepository) {
+    public UserServiceImpl(final UserRepository userRepository, final PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(final User user) {
-        //TODO: Encode Password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     @Override
     public Optional<User> getUserById(final Long id) {
         return this.userRepository.findById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        final User user = this.userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+                .password(user.getPassword())
+                .build();
     }
 }
