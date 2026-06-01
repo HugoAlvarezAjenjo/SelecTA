4e881a09bbaedde038bcfef6afbdfc71da68eaff
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2026-06-01 18:43:46 +0200

    ui(TG-64): Atributos ARIA

--- src/main/resources/static/js/mobile-menu.js
+++ src/main/resources/static/js/mobile-menu.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("DOMContentLoaded - mobile menu script funcionando");
     const mobileMenuButton = document.getElementById('mobile-menu-button');
     const mobileMenu = document.getElementById('mobile-menu');
 
     if (mobileMenuButton && mobileMenu) {
         mobileMenuButton.addEventListener('click', () => {
+            const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
             mobileMenu.classList.toggle('hidden');
+            mobileMenuButton.setAttribute('aria-expanded', String(!isExpanded));
+            mobileMenuButton.setAttribute('aria-label', isExpanded ? 'Abrir menú de navegación' : 'Cerrar menú de navegación');
         });
     }
 });
